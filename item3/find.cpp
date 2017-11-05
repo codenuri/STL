1814c4379ee7d46e82a03a7ac3c38f3e68ab0fbc
ecourse <smkang@codenuri.co.kr>
2017-11-05 14:40:12 +0900

    Add files via upload

---
+++ item3/find.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int x[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	int* p = find(x, x + 10, 5); // x ~ x+10 사이에서 5을 검색합니다.
+
+	cout << *p << endl;	// 5
+}
